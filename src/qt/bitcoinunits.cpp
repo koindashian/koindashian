8cbf7f4c1f739b33b46e5dcaa23789613cb7559b
Sylvain Blot <sylvainblot@me.com>
2014-01-09 13:16:12 +0100

    Renames

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("DOGE");
-    case mBTC: return QString("mDOGE");
-    case uBTC: return QString::fromUtf8("μDOGE");
+    case BTC: return QString("KOIN");
+    case mBTC: return QString("mKOIN");
+    case uBTC: return QString::fromUtf8("μKOIN");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("DogeCoins");
-    case mBTC: return QString("Milli-DogeCoins (1 / 1,000)");
-    case uBTC: return QString("Micro-DogeCoins(1 / 1,000,000)");
+    case BTC: return QString("Koindashians");
+    case mBTC: return QString("Milli-Koindashians (1 / 1,000)");
+    case uBTC: return QString("Micro-Koindashians(1 / 1,000,000)");
     default: return QString("???");
     }
 }
